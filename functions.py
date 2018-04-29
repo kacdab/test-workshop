1ce7f3a581f35df07a6fdd5ea09f930c02a697e7
Kacper Dąbrowski <office@kacperdabrowski.io>
2018-04-29 18:09:03 +0200

    Add a price function and tests

--- functions.py
+++ functions.py
@@ -3,3 +3,23 @@ def func(x):
 
 def multiplication(a, b):
     return a * b
+
+# Function price_calculation should return a price for a ticket.
+# If someone is young enough (<20) it should be 100
+# If someone is 20 exactly it should be 120
+# If someone is 21 or more but less than 65 it should be 150
+# If someone is older than 65 or 65 exactly it should be 200
+# An age is 0 or more but not less. The function should return None if age < 0
+
+def price_calculation(age):
+    if(age >= 0):
+        if(age < 20):
+            return 100
+        elif(age == 20):
+            return 120
+        elif(20 < age < 65):
+            return 150
+        else:
+            return 200
+    else:
+        return None
--- tests.py
+++ tests.py
@@ -1,4 +1,15 @@
-from functions import func
+from functions import *
 
 def test_func():
     assert func(3) == 4
+
+def test_multiplication():
+    assert multiplication(4, 8) == 32
+
+def test_price_calculation():
+    assert price_calculation(19) == 100
+    assert price_calculation(20) == 120
+    assert price_calculation(40) == 150
+    assert price_calculation(65) == 200
+    assert price_calculation(68) == 200
+    assert price_calculation(-1) == None
